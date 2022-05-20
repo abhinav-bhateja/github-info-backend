817fe5dbade489bad8346ae2a5ba3f51ff1f7263
Abhinav Bhateja <abhinav@valarisolutions.com>
2022-05-20 20:15:03 +0530

    Trying Railway ENV

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors())
 
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true})
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true})
 
 const db = mongoose.connection
 db.on('error', error => console.error(error))
